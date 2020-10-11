b23062ef53811a0fd33f8eb997d9c6c9156f2819
Muhaddatha Abdulghani <mzabdulg@umich.edu>
2020-10-10 22:01:49 -0400

    Created simple log in page

---
+++ index.php
@@ -0,0 +1,28 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>
+    <!-- Similar to require, makes sure the file is included. If the file is already included, it doesn't include it again. -->
+    <?php require_once "bootstrap.php"; ?>
+    </title>
+</head>
+
+<body>
+    <div class="container">
+        <h1>Welcome to rock paper scissors</h1>
+
+        <p>
+            <a href="login.php">Please Login</a>
+        </p>
+
+        <p>
+        Go to the
+        <a href="game.php">game.php</a>
+        </p>
+
+    </div>
+    
+</body>
+</html>
